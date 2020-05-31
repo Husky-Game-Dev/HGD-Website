b1db9dff632cade0626d57970de001608215d916
hollowsunsets <isabelgiang@gmail.com>
2020-05-31 16:12:13 -0700

    Update Game Jam section design, add Winter 2020 submissions

--- assets/js/init.js
+++ assets/js/init.js
@@ -79,37 +79,27 @@ jQuery( document ).ready(function( $ ) {
 		}	
 	});
 	
-	/*** 3D Gallery *********/
-	var gridGalleries = document.getElementsByClassName( 'grid-gallery' );
-	for (var i = 0; i < gridGalleries.length; i++) {
-		new CBPGridGallery(gridGalleries[i]);
-	}
-	// new CBPGridGallery( document.getElementsByClassName( 'grid-gallery' ) );
+	/****** Workshop Quarter Tabs *************/
 	new CBPFWTabs( document.getElementById( 'tabs-ui' ) );
 	
-	/********Vimeo Video*****************/
-	$('.venobox').venobox({
-		numeratio: true,
-		infinigall: true,
-		border: '20px'
+	/******* Slick Slider *******************/
+	$(document).ready(function() {
+		$('.game-jams').slick({
+			dots: true,
+			infinite: true,
+			speed: 500,
+			fade: true,
+			cssEase: 'linear'		});
 	});
-	$('.venoboxvid').venobox({
-		bgcolor: '#000'
-	});
-	$('.venoboxframe').venobox({
-		border: '6px'
-	});
-	$('.venoboxinline').venobox({
-		framewidth: '300px',
-		frameheight: '250px',
-		border: '6px',
-		bgcolor: '#f46f00'
-	});
-	$('.venoboxajax').venobox({
-		border: '30px;',
-		frameheight: '220px'
-	});	
-		
+
+	$(document).ready(function() {
+		$('.game-jam-submissions').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 2000
+		})
+	})
 		
 	/*******Schedule Accordion *************/
 	
@@ -204,76 +194,6 @@ jQuery( document ).ready(function( $ ) {
 	wow.init();
 		
 		
-
-	/** Registration Form **/
-	$("#submit_btn").click(function() 
-	{ 
-
-		var filter = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-		
-		var e = document.getElementById("field_1");
-		var user_type = e.options[e.selectedIndex].text;
-		
-	
-		var person_name = document.getElementById("name").value;
-		var user_email = document.getElementById("email1").value;
-		
-		var proceed = true;
-		
-		
-		if(person_name == ""){ 
-			var error1 = '<div class="enter-name col-lg-3 align-center"> Enter the name </div>';
-			jQuery("#result").hide().html(error1).fadeIn(500);
-			proceed = false;
-			return false;
-		}
-		
-		if(user_email== ""){
-			var error2 = '<div class="enter-email col-lg-3 align-center"> Enter the email </div>';
-			jQuery("#result").hide().html(error2).fadeIn(500);
-			proceed = false;
-			
-		}
-		else if(!filter.test(user_email)) {
-			var invalid = '<div class="invalid-email col-lg-3 align-center"> Invalid Email </div>';
-			jQuery("#result").hide().html(invalid).fadeIn(500);
-			proceed = false;
-			
-		}
-	
-		if (proceed) //everything looks good! proceed...
-		{
-			
-			//data to be sent to server
-			var post_data = {
-				'userName': person_name,
-				'userType': user_type,
-				'userEmail': user_email,
-
-			}
-			
-			//Ajax post data to server
-			jQuery.post('contact_me.php', post_data, function(response) {
-				//load json data from server and output message
-				if (response.type == 'error') {
-					var output = '<div class="error col-lg-3 align-center">' + response.text + '</div>';
-				} else {
-					var output = '<div class="success col-lg-3 align-center">' + response.text + '</div>';
-					//reset values in all input fields
-					
-				}
-				jQuery("#result").hide().html(output).fadeIn(500);
-				
-			}, 'json');
-		}
-
-		return false;
- 
-	});
-		
-	/** Subscribe JS **/
-	$("#notifyMe").notifyMe(); // Activate notifyMe plugin on a '#notifyMe' element 
-
 
 	/*** Overlay close*********/
 	$('.md-overlay').click(function(e){
